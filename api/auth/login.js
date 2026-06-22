b1774c58d965803011e001d53a25cf514981b9c0
padillaskies-lgtm <padillaskies@gmail.com>
2026-06-22 23:17:29 +0800

    Create login.js

---
+++ api/auth/login.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    response_type: 'code',
+    scope: 'identify guilds.members.read',
+  });
+
+  res.redirect(`https://discord.com/api/oauth2/authorize?${params}`);
+}
