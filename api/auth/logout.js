41296b4746b22b559eb877cbe9f5a1e286ed17a4
padillaskies-lgtm <padillaskies@gmail.com>
2026-06-24 08:47:54 +0800

    Delete api/auth directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-// /api/auth/logout.js
-// Clears the session cookie and redirects home
-
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', 'vv_session=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0');
-  res.redirect('/');
-}
